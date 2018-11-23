0c3c55f0d50fb0f4b4909604805775d96672c9d5
HsinYin <peter0930260535@gmail.com>
2018-11-23 23:02:39 +0800

    Implement mpi version of prime with mpi broadcast and reduce.

--- Lab3/prime_mpi.c
+++ Lab3/prime_mpi.c
@@ -20,109 +20,42 @@ int main(int argc, char *argv[])
 {
 	int pc,       /* prime counter */
 		foundone, /* most recent prime found */
-		locRank,
-		size,
+		rank,
+		procNum,
 		partialPC = 0,
-		partialFO = 0,
-		locPC = 0,
-		tag = 0;
+		partialFO = 0;
 	long long int n=0, limit, avgWork, remainWork, start, end;
-	MPI_Status status;
 
 	MPI_Init( &argc, &argv );
-	MPI_Comm_rank( MPI_COMM_WORLD, &locRank );
-	MPI_Comm_size( MPI_COMM_WORLD, &size );
+	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+	MPI_Comm_size( MPI_COMM_WORLD, &procNum );
 
-	if( locRank == 0 ){
+	if( rank == 0 ){
 		sscanf(argv[1],"%llu",&limit);	
 		printf("Starting. Numbers to be scanned= %lld\n",limit);
 	}
 
-	pc=4;     /* Assume (2,3,5,7) are counted here */
-	avgWork = (limit - 11 + 1) / size;
-	remainWork = (limit - 11 + 1) % size;
+	MPI_Bcast( &limit, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD );
 
-	if( locRank == 0 ) {
-		for( int id=1; id < size; id++ ) {
-			if( id < remainWork ) {
-				//start = 11 + (id-1)*avgWork + id - 1;
-				//end = 11 + id*avgWork + id - 1;
-				start = 11 + id*avgWork + id;
-				end = 11 + (id+1)*avgWork + (id-1);
-			} else {
-				//start = 11 + (id-1)*avgWork + remainWork;
-				//end = 11 + id*avgWork + remainWork - 1;
-				start = 11 + id*avgWork + remainWork;
-				end = 11 + (id+1)*avgWork + remainWork - 1;
-			}
-			MPI_Send( &start, 1, MPI_LONG_LONG, id, tag, MPI_COMM_WORLD );
-			MPI_Send( &end, 1, MPI_LONG_LONG, id, tag, MPI_COMM_WORLD );
-			MPI_Send( &avgWork, 1, MPI_LONG_LONG, id, tag, MPI_COMM_WORLD );
-			MPI_Send( &remainWork, 1, MPI_LONG_LONG, id, tag, MPI_COMM_WORLD );
-		}
-
-
-		if( locRank < remainWork ) {
-			//start = 11 + (id-1)*avgWork + id - 1;
-			//end = 11 + id*avgWork + id - 1;
-			start = 11 + locRank*avgWork;
-			end = 11 + (locRank+1)*avgWork;
-		} else {
-			//start = 11 + (id-1)*avgWork + remainWork;
-			//end = 11 + id*avgWork + remainWork - 1;
-			start = 11 + locRank*avgWork;
-			end = 11 + (locRank+1)*avgWork - 1;
-		}
-
-		for( long long int i = start; i <= end; i++ ) {
-			if( i % 2 == 0 )
-				continue;
-			if( isprime(i) ) {
-				pc++;
-				if( i > foundone )
-					foundone = i;
-			}
-		}
-
-		for( int i=1; i < size; i++ ) {
-			MPI_Recv( &partialFO, 1, MPI_INT, i, tag, MPI_COMM_WORLD,
-					&status );
-			MPI_Recv( &partialPC, 1, MPI_INT, i, tag, MPI_COMM_WORLD,
-					&status );
-			//printf("received partial fo: %d, partial pc: %d\n", partialFO,
-			//		partialPC);
-			pc += partialPC;
-			if( partialFO > foundone )
-				foundone = partialFO;
+	for( n = 11+2*rank; n <= limit; n += 2*procNum ) {
+		/********************************************************************
+		 * n must be an odd number starting from 11.
+		 ********************************************************************/
+		if( isprime( n ) ) {
+			partialPC++;
+			partialFO = n;
 		}
 	}
-	else {
 
-		MPI_Recv( &start, 1, MPI_LONG_LONG, 0, tag, MPI_COMM_WORLD, &status );
-		MPI_Recv( &end, 1, MPI_LONG_LONG, 0, tag, MPI_COMM_WORLD, &status );
-		//MPI_Recv( &avgWork, 1, MPI_LONG_LONG, 0, tag, MPI_COMM_WORLD, &status );
-		//MPI_Recv( &remainWork, 1, MPI_LONG_LONG, 0, tag, MPI_COMM_WORLD, &status );
+	MPI_Reduce( &partialPC, &pc, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD );
+	if( rank == 0 )
+		pc += 4;	/* Assume (2,3,5,7) are counted here */
+	MPI_Reduce( &partialFO, &foundone, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD );
 
-		//printf("rank:%d, start: %lld, end: %lld, avgWork:%lld, remain:%lld, pc:%d, foundone:%d\n", 
-		//		locRank, start, end, avgWork, remainWork, partialPC, partialFO );
-		for( long long int i = start; i <= end; i++ ) {
-			if( i % 2 == 0 )
-				continue;
-			if( isprime(i) ){
-				partialPC++;
-				partialFO = i;
-			}
-		}
-		//printf("rank %d's partial foundone: %d; parital pc: %d\n",
-		//		locRank, partialFO, partialPC);
 
-		MPI_Send( &partialFO, 1, MPI_INT, 0, tag, MPI_COMM_WORLD );
-		MPI_Send( &partialPC, 1, MPI_INT, 0, tag, MPI_COMM_WORLD );
-
-	}
-
-	if( locRank == 0 )
+	if( rank == 0 )
 		printf("Done. Largest prime is %d Total primes %d\n",foundone,pc);
+
 	MPI_Finalize();
 
 	return 0;
